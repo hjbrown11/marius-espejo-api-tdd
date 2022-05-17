1865adffc7c150bdea8f8ed312d912ac4c00f217
Hannah Brown <hannah.j.brown@hotmail.co.uk>
2022-05-17 11:49:03 +0100

    Add get routes to pass tests

---
+++ marius-espejo-api-tdd/routes/todos.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const createError = require('http-errors');
+
+const todos = [{id: 1, name: 'Do something', completed: false}];
+
+router.get('/', function(req, res, next) {
+  res.json(todos);
+});
+
+router.get('/:id', function(req, res, next) {
+  const foundTodo = todos.find(todo => todo.id === Number(req.params.id));
+  if(!foundTodo) {
+    return next(createError(404, 'Not Found'));
+  }
+  res.json(foundTodo);
+});
+
+module.exports = router;
